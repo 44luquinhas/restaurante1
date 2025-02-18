76b692302d322882285ad220d502319f864d4ccb
44luquinhas <159542226+44luquinhas@users.noreply.github.com>
2025-02-18 09:55:46 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-print('lucacus food\n')
+import os
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante ')
-print('3. Ativar restaurante')
-print('4. sair')
+def exibir_nome_do_programa():
+        print('lucacus food\n')
+
+def exibir_opcoes():
+        print('1. Cadastrar restaurante')
+        print('2. Listar restaurante ')
+        print('3. Ativar restaurante')
+        print('4. sair')
 
 opcao_escolhida =int(input('Escolha o opção: '))
 print(f'Você escolheu a opção {opcao_escolhida}')
+
+def finalizar_app():
+    os.system('cls')
+    print('Finalizar o app')
 
 if opcao_escolhida == 1:
     print('Cadastrar restaurante')
@@ -15,6 +23,13 @@ elif opcao_escolhida == 2:
 elif opcao_escolhida == 3:
         print('Ativar restaurante')
 else:
-        print('Encerrando programa')
+        finalizar_app()
+
+def main():
+        exibir_nome_do_programa()
+        exibir_opcoes()
+
+        if __name__ == '__main__':
+                main()
 
 
